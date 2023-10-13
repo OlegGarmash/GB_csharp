66eada8bebd7046c6f7334b00d45bcd33ac69a09
OlegGarmash <oleg@stavrol.ru>
2023-10-13 10:29:49 +0400

    Мелкая корретировка в 19 таске

--- task19_Палиндром/Program.cs
+++ task19_Палиндром/Program.cs
@@ -6,7 +6,7 @@
 System.Console.WriteLine("Введите пятизначное число:");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number > 9999 || number < 100000)
+if (number > 9999 && number < 100000)
 {
     if (number / 10000 == number % 10 && number / 1000 % 10 == number / 10 % 10)
         System.Console.WriteLine($"{number} -> True");
