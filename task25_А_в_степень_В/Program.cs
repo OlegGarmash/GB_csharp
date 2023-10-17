074fbb9a59d8558b305f727d8cb8e346c16dbf60
OlegGarmash <oleg@stavrol.ru>
2023-10-17 13:01:35 +0400

    Добавил проверок в 25 таск.

--- task25_А_в_степень_В/Program.cs
+++ task25_А_в_степень_В/Program.cs
@@ -10,26 +10,40 @@ int userA = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите степень числа:");
 int userB = Convert.ToInt32(Console.ReadLine());
 
+while (userB < 1)
+{
+    System.Console.WriteLine("Степень не натуральное число!");
+    System.Console.WriteLine("Введите степень числа:");
+    userB = Convert.ToInt32(Console.ReadLine());
+}
+
+/*if (userB < 1)
+    System.Console.WriteLine("Степень не натуральное число!");
+
+else
+{*/
 double PowerUp(int a, int b)
 {
     double result = 1;
     if (b > 0)
+
     {
         for (int i = 0; i < b; i++)
         {
             result = result * a;
         }
     }
-    else if (b < 0)
+    /*else if (b < 0)
     {
         b = b * -1;
         for (int i = 0; i < b; i++)
         {
             result = result / a;
         }
-    }
+    }*/
     return result;
 }
 
 double userResult = PowerUp(userA, userB);
 System.Console.WriteLine($"{userA}, {userB} -> {userResult}");
+//}
