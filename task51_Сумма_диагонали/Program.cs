b4d22be2cd3e64f223864cb50d4ac50c5e9e2bc7
OlegGarmash <oleg@stavrol.ru>
2023-10-28 21:49:21 +0400

    Таск 51-52

--- task51_Сумма_диагонали/Program.cs
+++ task51_Сумма_диагонали/Program.cs
@@ -6,3 +6,64 @@
 8 4 2 4 
 Сумма элементов главной диагонали: 1+9+2 = 12
 */
+
+int Prompt(string massage)
+{
+    System.Console.Write(massage);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    int result = Convert.ToInt32(Console.ReadLine());
+    Console.ResetColor();
+    return result;
+}
+
+int[,] GenerateMatrix(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+
+int DiagonalSum(int[,] matrixOne)
+{
+    int sum = 0;
+    for (int i = 0; i < matrixOne.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixOne.GetLength(1); j++)
+        {
+            if (i == j)
+            sum = sum + matrixOne[i, j];
+        }
+    }
+    return sum;
+}
+
+void PrintMatrix(int[,] matrixOne, int sum)
+{
+    System.Console.WriteLine("\nДвумерный массив:");
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    for (int i = 0; i < matrixOne.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixOne.GetLength(1); j++)
+        {
+            System.Console.Write(matrixOne[i, j]);
+            if (j < matrixOne.GetLength(1) - 1)
+                System.Console.Write("\t");
+            else System.Console.WriteLine();
+        }
+    }
+    Console.ResetColor();
+    System.Console.WriteLine($"\nСумма главной диагонали равна: {sum}");
+}
+
+int rows = Prompt("Количество строк: ");
+int columns = Prompt("Количество столбцов: ");
+int min = Prompt("Минимальное значение: ");
+int max = Prompt("Максимальное значене: ");
+int[,] matrix = GenerateMatrix(rows, columns, min, max);
+PrintMatrix(matrix, DiagonalSum(matrix));
--- task52_Среднее_в_столбце/Program.cs
+++ task52_Среднее_в_столбце/Program.cs
@@ -7,3 +7,62 @@
 8 4 2 4 
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+
+int Prompt(string massage)
+{
+    System.Console.Write(massage);
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    int result = Convert.ToInt32(Console.ReadLine());
+    Console.ResetColor();
+    return result;
+}
+
+double[,] GenerateMatrix(int rows, int columns, int minValue, int maxValue)
+{
+    double[,] matrix = new double[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            matrix[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return matrix;
+}
+
+double[] ColumnsAverage(int[,] matrixOne)
+{
+    double[] average = new double[matrixOne.GetLength(0)];
+    for (int i = 0; i < matrixOne.GetLength(1); i++)
+    {
+        for (int j = 0; j < matrixOne.GetLength(0); j++)
+        {
+            average[j] = matrixOne[j] + average[j];
+        }
+    }
+    return average;
+}
+
+void PrintMatrix(double[,] matrixOne)
+{
+    System.Console.WriteLine("\nДвумерный массив:");
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    for (int i = 0; i < matrixOne.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixOne.GetLength(1); j++)
+        {
+            System.Console.Write(matrixOne[i, j]);
+            if (j < matrixOne.GetLength(1) - 1)
+                System.Console.Write("\t");
+            else System.Console.WriteLine();
+        }
+    }
+    Console.ResetColor();
+}
+
+int rows = Prompt("Количество строк: ");
+int columns = Prompt("Количество столбцов: ");
+int min = Prompt("Минимальное значение: ");
+int max = Prompt("Максимальное значене: ");
+double[,] matrix = GenerateMatrix(rows, columns, min, max);
+PrintMatrix(matrix);
